6717f3b7a28102425378b9538dd122714a31e980
zedfaceless <zedfaces.fernandez@gmail.com>
2024-07-10 22:27:59 +0800

    upload new file

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,42 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        DisplayWelcome();
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 3, 6),
+            new Circle("Green", 5)
+        };
 
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
-        int squaredNumber = SquareNumber(userNumber);
-
-        DisplayResult(userName, squaredNumber);
-    }
-
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to the program!");
-    }
-
-    static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        return Console.ReadLine();
-    }
-
-    static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        return int.Parse(Console.ReadLine());
-    }
-
-    static int SquareNumber(int number)
-    {
-        return number * number;
-    }
-
-    static void DisplayResult(string userName, int squaredNumber)
-    {
-        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
